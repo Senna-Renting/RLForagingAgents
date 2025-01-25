189f057cd39cc654963a2f07f7a51cc973b5c8ce
Senna Renting <senna.renting@gmail.com>
2025-01-25 12:40:43 +0100

    Removing cached files

--- DDPG_Incremental/.ipynb_checkpoints/welfare_functions-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-# Welfare functions will be put below
-def nash_sw(rewards):
-    res = 1
-    for r in rewards:
-        res *= max(0,r) # No negative numbers allowed
-    return res
-def mean_sw(rewards):
-    return np.mean(rewards)
